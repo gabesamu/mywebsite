a08c6615faa9e2e6d2ce49f207cefeb7f123a20d
Lucius <gmsamuel18@gmail.com>
2024-03-04 15:15:08 -0600

    initial commit

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let title = 'Blog Page';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h2>{title}</h2>
+
+<style lang="scss">
+	h2 {
+		color: hotpink;
+	}
+</style>
